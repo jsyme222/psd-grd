9eb4b44e41a7d01d471df07bbcfedeb2c4debbfe
jsyme222 <jsyme@gmail.com>
2020-05-18 09:37:06 -0600

    Removed auto download

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ function start_parse(stream_reader){
 
             timeoutFn = setTimeout(function () {
                 parse();
-                downloadAllCSS();
+                // downloadAllCSS();
             }, 0);
         }
     };
